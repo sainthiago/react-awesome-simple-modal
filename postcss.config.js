e5acd4b87598b366f341efda3ad20255212f66e4
Rui Santiago <ruisantiagomr@gmail.com>
2022-09-01 11:21:59 +0100

    Update Tailwind configs

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+import postcss from "rollup-plugin-postcss";
+
+module.exports = {
+  plugins: [
+    postcss({
+      config: {
+        path: "./postcss.config.js",
+      },
+      extensions: [".css"],
+      minimize: true,
+      inject: {
+        insertAt: "top",
+      },
+    }),
+  ],
+};
